fdb61b1f386985050918c1a577a8a7b0a450de02
RaviTejGuntuku <tej.guntuku@gmail.com>
2024-10-03 11:21:11 -0500

    more code

--- FlyweightedClass.java
+++ FlyweightedClass.java
@@ -1,5 +1,112 @@
+import java.util.*;
+
+// Replace the Trees and MyTrees class for String, and you start to understand how String and StringPool works.
+class Trees {
+
+    // Can't
+    public static final class MyTree {
+        private final int x;
+        private final int y;
+
+        public MyTree(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+
+            if (o.getClass() == getClass()) {
+                MyTree m = (MyTree) o;
+                return m.x == x && m.y == y;
+            }
+            return false;
+        }
+    }
+
+    private static Map<String, MyTree> map = new HashMap<>();
+
+    public static MyTree makeNewTree(int x, int y) {
+        String point = x + ", " + y;
+
+        if (!map.containsKey(point)) {
+            map.put(point, new MyTree(x, y));
+        }
+
+        return map.get(point);
+    }
+
+}
+
+// This code doesn't even use a nested class structure and still implements
+// Flyweighting, much like the String class (it's a great analogy)
+final class StandaloneTree {
+
+    private int x;
+    private int y;
+
+    private static Map<String, StandaloneTree> map = new HashMap<>();
+
+    private StandaloneTree(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static StandaloneTree makeNewTree(int x, int y) {
+
+        String point = x + ", " + y;
+
+        if (!map.containsKey(point)) {
+            map.put(point, new StandaloneTree(x, y));
+        }
+
+        return map.get(point);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public StandaloneTree intern() {
+        return makeNewTree(x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (o.getClass() == getClass()) {
+            StandaloneTree m = (StandaloneTree) o;
+            return m.x == x && m.y == y;
+        }
+        return false;
+    }
+
+}
+
 public class FlyweightedClass {
     public static void main(String[] args) {
+
+        Trees.MyTree n = Trees.makeNewTree(1, 5);
+
+        Trees.MyTree m = new Trees.MyTree(1, 5);
+
+        Trees.MyTree o = Trees.makeNewTree(1, 5);
+
+        System.out.println(n == m);
+        System.out.println(n == o);
+        System.out.println(m == o);
+
+        System.out.println();
+
+        System.out.println(n.equals(m));
+        System.out.println(n.equals(o));
+        System.out.println(m.equals(o));
+
+        System.out.println(n.getClass());
 
     }
 }
---
+++ StringInterning.java
@@ -0,0 +1,66 @@
+class StringStorage {
+
+    String s1;
+    String s2;
+
+    public StringStorage(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+}
+
+public class StringInterning {
+
+    static String firstPart = "hel";
+    static String secondPart = "lo";
+
+    public static void main(String[] args) {
+
+        // Obvious cases
+        String s1 = "hello";
+        String s2 = "hello";
+
+        String s3 = new String("hello");
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+
+        System.out.println();
+
+        // What do these methods below output?
+        String int1 = Integer.toString(5);
+        String int2 = Integer.toString(5);
+        System.out.println(int1 == int2);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+
+        System.out.println(sb.toString() == s1);
+
+        String newHello = firstPart + secondPart;
+        System.out.println(newHello == s1);
+
+        newHello = returnPart1() + returnPart1();
+        System.out.println(newHello == s1);
+
+        StringStorage s = new StringStorage("h", "ello");
+        newHello = s.s1 + s.s2;
+
+        System.out.println(newHello == s1);
+
+    }
+
+    public static String returnPart1() {
+        return "hel";
+    }
+
+    public static String returnPart2() {
+        return "lo";
+    }
+}
